adf5653bcb1898a4989af40514c435bde5325973
Jayant-stack <jayanthalapati@gmail.com>
2025-01-18 20:50:58 -0500

    fixing white label error

--- src/main/java/com/jayanth/tradingplatform/config/JwtConstant.java
+++ src/main/java/com/jayanth/tradingplatform/config/JwtConstant.java
@@ -2,5 +2,5 @@ package com.jayanth.tradingplatform.config;
 
 public class JwtConstant {
     public static final String SECRET = "your-very-long-secret-key-at-least-32-characters";
-    public static final String jwtHeader = "AUTHORIZATION";
+    public static final String jwtHeader = "Authorization";
 }
